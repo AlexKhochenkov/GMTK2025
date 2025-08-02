43db3a28ee410b3a5b1a7738e69167d181dbbf4d
Алексей Хоченков <alekshoch@yandex.ru>
2025-08-02 19:48:00 +0300

    sound jump

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -19,6 +19,8 @@ public class PlayerController : MonoBehaviour
 
     private Rigidbody2D _rb;
     private Animator _animator;
+    private AudioSource _audioSource;
+    private bool _jumpSoundPlayed = false;
     private bool _isJumping;
     private float _jumpTimeCounter;
     private float _coyoteTimeCounter;
@@ -26,6 +28,7 @@ public class PlayerController : MonoBehaviour
 
     void Start()
     {
+        _audioSource = GetComponent<AudioSource>();
         _animator = GetComponent<Animator>();
         _rb = GetComponent<Rigidbody2D>();
         _rb.freezeRotation = true;
@@ -36,6 +39,7 @@ public class PlayerController : MonoBehaviour
         HandleCoyoteTime();
         HandleJumpBuffer();
         HandleJump();
+        HandleJumpSound();
         HandleGravity();
         UpdateAnimations();
     }
@@ -71,18 +75,20 @@ public class PlayerController : MonoBehaviour
 
     private void HandleJump()
     {
-        // Start jump
         if (_jumpBufferCounter > 0 && _coyoteTimeCounter > 0)
         {
             _isJumping = true;
             _jumpTimeCounter = maxJumpTime;
             _rb.linearVelocity = new Vector2(_rb.linearVelocity.x, jumpForce);
             _jumpBufferCounter = 0;
-
+            if (!_jumpSoundPlayed)
+            {
+                _audioSource.Play();
+                _jumpSoundPlayed = true;
+            }
             if (jumpParticles) jumpParticles.Play();
         }
 
-        // Hold jump for variable height
         if (Input.GetButton("Jump") && _isJumping)
         {
             if (_jumpTimeCounter > 0)
@@ -96,7 +102,6 @@ public class PlayerController : MonoBehaviour
             }
         }
 
-        // Release jump button
         if (Input.GetButtonUp("Jump"))
         {
             _isJumping = false;
@@ -120,6 +125,14 @@ public class PlayerController : MonoBehaviour
     private void CheckGrounded()
     {
         _animator.SetBool("Grounded", IsGrounded());
+    }
+
+    private void HandleJumpSound()
+    {
+        if (IsGrounded() && _jumpSoundPlayed)
+        {
+            _jumpSoundPlayed = false;
+        }
     }
 
     private bool IsGrounded()
